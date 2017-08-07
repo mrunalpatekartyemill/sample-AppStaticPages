5eb3b15621143db4c68fe15f0ac7a73dcaa852fd
mrunalpatekartyemill <mrunal@tyemill.com>
2017-08-07 11:05:42 -0700

    filling layouts and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-
-  get 'static_pages/help'
-
-  get 'static_pages/about'
 
   root 'static_pages#home'
 
-end
+  get 'users/new'
+
+  get 'static_pages/home',  to:   'static_pages#home'
+
+  get 'static_pages/help',   to:   'static_pages#help'
+
+  get 'static_pages/about',  to: 'static_pages#about'
+
+  get '/signup' , to: 'users#new'
+
+  end
